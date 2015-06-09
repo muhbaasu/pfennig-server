c0b8c1459515b5a815f2eaf8a2dcefb460c9a06f
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2015-06-09 22:03:26 +0200

    Add basic implementation of `DataKind'ed refs

--- src/Pfennig/Handlers.hs
+++ src/Pfennig/Handlers.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE QuasiQuotes         #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -7,9 +8,12 @@ import           App
 import           Control.Error.Safe        (justErr)
 import           Control.Monad             (join)
 import           Data.Aeson                (object, (.=))
+import           Data.Aeson                (object, (.=))
 import           Data.Bifunctor            (bimap)
 import qualified Data.Text                 as T
+import qualified Data.Text                 as T
 import           Data.Text.Encoding        (decodeUtf8)
+import           Data.Time.Format          (defaultTimeLocale, parseTimeOrError)
 import           Data.Time.LocalTime       (LocalTime)
 import           Data.Word                 (Word64)
 import qualified Hasql                     as H
@@ -19,96 +23,114 @@ import           Network.HTTP.Types.Status
 import           Web.Scotty                (ActionM, json, jsonData, param,
                                             status)
 
-unpackCxError :: HP.CxError -> T.Text
-unpackCxError (HP.CantConnect err) = maybe (T.pack "Unable to connect") decodeUtf8 err
-unpackCxError (HP.UnsupportedVersion ver) = T.pack $ "Postgres version " ++ show ver ++ " is not supported."
+-- <<<<<<< HEAD
+-- unpackCxError :: HP.CxError -> T.Text
+-- unpackCxError (HP.CantConnect err) = maybe (T.pack "Unable to connect") decodeUtf8 err
+-- unpackCxError (HP.UnsupportedVersion ver) = T.pack $ "Postgres version " ++ show ver ++ " is not supported."
 
-unpackTxError :: HP.TxError -> T.Text
-unpackTxError _ = T.pack "Transaction error"
+-- unpackTxError :: HP.TxError -> T.Text
+-- unpackTxError _ = T.pack "Transaction error"
 
-unpackSessionError :: H.SessionError HP.Postgres -> T.Text
-unpackSessionError (H.CxError err) = unpackCxError err
-unpackSessionError (H.TxError err) = unpackTxError err
-unpackSessionError (H.ResultError txt) = txt
+-- unpackSessionError :: H.SessionError HP.Postgres -> T.Text
+-- unpackSessionError (H.CxError err) = unpackCxError err
+-- unpackSessionError (H.TxError err) = unpackTxError err
+-- unpackSessionError (H.ResultError txt) = txt
 
-insertExpenditure :: ExpenditureFields -> H.Tx HP.Postgres s Expenditure
-insertExpenditure (ExpenditureFields description) = do
-  row <-
-    H.singleEx $ [H.stmt|insert into expenditures (description)
-                         values (?) returning *|] description
-  return $ rowToExpenditure row
+-- insertExpenditure :: ExpenditureFields -> H.Tx HP.Postgres s Expenditure
+-- insertExpenditure (ExpenditureFields description) = do
+-- =======
+-- insertExpenditure :: (ExpenditureFields 'Database)
+--                   -> H.Tx HP.Postgres s (Expenditure 'Database)
+-- insertExpenditure (ExpenditureFields description _) = do
+-- >>>>>>> Add basic implementation of `DataKind'ed refs
+--   row <-
+--     H.singleEx $ [H.stmt|insert into expenditures (description)
+--                          values (?) returning *|] description
+--   return $ rowToExpenditure row
 
-allExpenditures :: H.Tx HP.Postgres s [Expenditure]
-allExpenditures = do
-  rows <-
-    H.listEx $ [H.stmt|select * from expenditures|]
-  return $ fmap rowToExpenditure rows
+-- allExpenditures :: H.Tx HP.Postgres s [Expenditure 'Database]
+-- allExpenditures = do
+--   rows <-
+--     H.listEx $ [H.stmt|select * from expenditures|]
+--   return $ fmap rowToExpenditure rows
 
-singleExpenditure :: ExpenditureId -> H.Tx HP.Postgres s (Maybe Expenditure)
-singleExpenditure (ExpenditureId eid) = do
-  row <-
-     H.maybeEx $ [H.stmt|select * from expenditures where id = ?|] eid
-  return $ fmap rowToExpenditure row
+-- <<<<<<< HEAD
+-- singleExpenditure :: ExpenditureId -> H.Tx HP.Postgres s (Maybe Expenditure)
+-- singleExpenditure (ExpenditureId eid) = do
+--   row <-
+--      H.maybeEx $ [H.stmt|select * from expenditures where id = ?|] eid
+--   return $ fmap rowToExpenditure row
 
-delExpenditure :: ExpenditureId -> H.Tx HP.Postgres s Word64
-delExpenditure (ExpenditureId eid) = do
-  count <-
-          H.countEx $ [H.stmt|delete from expenditures where id = ?|] eid
-  return count
+-- delExpenditure :: ExpenditureId -> H.Tx HP.Postgres s Word64
+-- delExpenditure (ExpenditureId eid) = do
+--   count <-
+--           H.countEx $ [H.stmt|delete from expenditures where id = ?|] eid
+--   return count
 
 
-rowToExpenditure :: (Int, LocalTime, LocalTime, T.Text) -> Expenditure
-rowToExpenditure (id', created, updated, desc) =
-  let eid = ExpenditureId id'
-      fields = ExpenditureFields desc
-  in Expenditure eid created updated fields
+-- rowToExpenditure :: (Int, LocalTime, LocalTime, T.Text) -> Expenditure
+-- =======
+-- rowToExpenditure :: (Int, LocalTime, LocalTime, T.Text) -> Expenditure 'Database
+-- >>>>>>> Add basic implementation of `DataKind'ed refs
+-- rowToExpenditure (id', created, updated, desc) =
+--   let eid = ExpenditureId id'
+--       fields = ExpenditureFields desc []
+--   in Expenditure eid created updated fields
 
-getExpenditures :: RouteHandler
-getExpenditures (AppConfig s) = do
-  dbResult <- s $ H.tx Nothing allExpenditures
-  case dbResult of
-    (Left err) -> do
-      json $ object [ "error" .= show err ]
-      status badRequest400
-    (Right expenditure) -> json expenditure
+-- getExpenditures :: RouteHandler
+-- getExpenditures (AppConfig s) = do
+--   dbResult <- s $ H.tx Nothing allExpenditures
+--   case dbResult of
+--     (Left err) -> do
+--       json $ object [ "error" .= show err ]
+--       status badRequest400
+--     (Right expenditure) -> json expenditure
 
-getExpenditure :: RouteHandler
-getExpenditure (AppConfig s) = do
-  eid <- param "id"
-  expenditure <- fmap mapErrText $ s $ H.tx Nothing $ singleExpenditure $ ExpenditureId eid
-  case expenditure of
-    (Left err) -> do
-      json $ object [ "error" .= show err ]
-      status notFound404
-    (Right ex) ->json ex
-  where mapErrText = join . bimap unpackSessionError (justErr "Not found")
+-- getExpenditure :: RouteHandler
+-- <<<<<<< HEAD
+-- getExpenditure (AppConfig s) = do
+--   eid <- param "id"
+--   expenditure <- fmap mapErrText $ s $ H.tx Nothing $ singleExpenditure $ ExpenditureId eid
+--   case expenditure of
+--     (Left err) -> do
+--       json $ object [ "error" .= show err ]
+--       status notFound404
+--     (Right ex) ->json ex
+--   where mapErrText = join . bimap unpackSessionError (justErr "Not found")
+-- =======
+-- getExpenditure =
+--   return $ do
+--     eid <- param "id"
+--     json $ Expenditure (ExpenditureId eid) randomLocalTime randomLocalTime fields
+--   where fields = ExpenditureFields "Hello!" []
+-- >>>>>>> Add basic implementation of `DataKind'ed refs
 
-createExpenditure :: RouteHandler
-createExpenditure (AppConfig s) = do
-    fields <- jsonData :: ActionM ExpenditureFields
-    dbResult <- s $ H.tx Nothing (insertExpenditure fields)
-    case dbResult of
-      (Left err) -> do
-        json $ object [ "error" .= show err ]
-        status badRequest400
-      (Right expenditure) -> json expenditure
+-- createExpenditure :: RouteHandler
+-- createExpenditure (AppConfig s) = do
+--     fields <- jsonData :: ActionM (ExpenditureFields 'Database)
+--     dbResult <- s $ H.tx Nothing (insertExpenditure fields)
+--     case dbResult of
+--       (Left err) -> do
+--         json $ object [ "error" .= show err ]
+--         status badRequest400
+--       (Right expenditure) -> json expenditure
 
-updateExpenditure :: RouteHandler
-updateExpenditure =
-  return $ do
-    _ <- param "id" :: ActionM Integer
-    expenditure <- jsonData :: ActionM ExpenditureFields
-    json expenditure
-    status accepted202
+-- updateExpenditure :: RouteHandler
+-- updateExpenditure =
+--   return $ do
+--     _ <- param "id" :: ActionM Integer
+--     expenditure <- jsonData :: ActionM (ExpenditureFields 'Database)
+--     json expenditure
+--     status accepted202
 
-deleteExpenditure :: RouteHandler
-deleteExpenditure (AppConfig s) = do
-  eid <- param "id" :: ActionM Int
-  count <- s $ H.tx Nothing $ delExpenditure $ ExpenditureId eid -- count should probably be checked
-  case count of
-    (Right 1) -> status accepted202
-    (Right 0) -> status notFound404
-    (Left err) -> do
-      json $ object [ "error" .= show err ]
-      status badRequest400
-    _ -> status internalServerError500 -- something weird happened
+-- deleteExpenditure :: RouteHandler
+-- deleteExpenditure (AppConfig s) = do
+--   eid <- param "id" :: ActionM Int
+--   count <- s $ H.tx Nothing $ delExpenditure $ ExpenditureId eid -- count should probably be checked
+--   case count of
+--     (Right 1) -> status accepted202
+--     (Right 0) -> status notFound404
+--     (Left err) -> do
+--       json $ object [ "error" .= show err ]
+--       status badRequest400
+--     _ -> status internalServerError500 -- something weird happened
--- src/Pfennig/Models.hs
+++ src/Pfennig/Models.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DataKinds    #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Models where
 
 import           Control.Monad       (mzero)
@@ -6,24 +9,36 @@ import           Data.Text           (Text)
 import           Data.Time.Format    (defaultTimeLocale, formatTime)
 import           Data.Time.LocalTime (LocalTime)
 
+data Reference = Database
+               | REST
+               | NoRef
+
+type family RefType (a :: *) (r :: Reference) :: * where
+  RefType (Expenditure r) Database = ExpenditureId
+  RefType a NoRef = a
+
 newtype ExpenditureId = ExpenditureId Int deriving (Eq, Show)
 newtype ExpenditureTagId = ExpenditureTagId Int deriving (Eq, Show)
 
 -- | batch of expenses at a specific point in time
-data Expenditure = Expenditure {
-       _expId        :: ExpenditureId
-     , _expCreatedAt :: LocalTime
-     , _expUpdatedAt :: LocalTime
-     , _expFields    :: ExpenditureFields
-     } deriving Show
+data Expenditure (r :: Reference)=
+  Expenditure {
+      _expId        :: ExpenditureId
+    , _expCreatedAt :: LocalTime
+    , _expUpdatedAt :: LocalTime
+    , _expFields    :: ExpenditureFields r
+    } deriving Show
 
 -- | a single expense description e.g. "cellphone bill"
-data ExpenditureFields = ExpenditureFields {
-       _expFldDescription :: Text
-     } deriving (Show)
+data ExpenditureFields (r :: Reference) =
+  ExpenditureFields {
+      _expFldDescription :: Text
+    , _expFldTags        :: [ExpenditureTag r]
+    } deriving (Show)
 
 -- | a tag to group the expenditures by
-data ExpenditureTag = ExpenditureTag {
+data ExpenditureTag (r :: Reference) =
+  ExpenditureTag {
        _tagId        :: ExpenditureTagId
      , _tagTitle     :: Text
      , _tagCreatedAt :: LocalTime
@@ -32,18 +47,18 @@ data ExpenditureTag = ExpenditureTag {
 instance ToJSON ExpenditureId where
   toJSON (ExpenditureId eid) = toJSON eid
 
-instance ToJSON Expenditure where
+instance ToJSON (Expenditure r) where
   toJSON label = object [ "id" .= _expId label
                         , "createdAt" .= _expCreatedAt label
                         , "updatedAt" .= _expUpdatedAt label
                         , "description" .= desc ]
     where desc = _expFldDescription $ _expFields label
 
-instance ToJSON ExpenditureFields where
-  toJSON (ExpenditureFields desc) = object [ "description" .= desc ]
+instance ToJSON (ExpenditureFields r) where
+  toJSON (ExpenditureFields desc tags) = object [ "description" .= desc ]
 
-instance FromJSON ExpenditureFields where
-  parseJSON (Object v) = ExpenditureFields <$> v .: "description"
+instance FromJSON (ExpenditureFields r) where
+  parseJSON (Object v) = ExpenditureFields <$> v .: "description" <*> pure []
   parseJSON _ = mzero
 
 instance ToJSON LocalTime where
