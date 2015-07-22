c1868b5e15f72fefed16df34b8ac609641128e6b
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2015-07-22 23:17:52 +0200

    Add NBF and EXP dates to JWT when logging in

--- src/Pfennig/Auth.hs
+++ src/Pfennig/Auth.hs
@@ -1,19 +1,28 @@
 {-# LANGUAGE DataKinds #-}
 module Auth where
 
+import           Control.Monad.IO.Class    (liftIO)
 import qualified Data.ByteString.Lazy      as BS
 import           Data.Monoid
 import           Data.Text
 import qualified Data.Text.Lazy            as TL
 import qualified Data.Text.Lazy.Encoding   as TL
 import           Data.Time.Calendar        (fromGregorian)
-import           Data.Time.Clock           (UTCTime (..), addUTCTime,
-                                            getCurrentTime, secondsToDiffTime)
+import           Data.Time.Clock           (NominalDiffTime, UTCTime (..),
+                                            addUTCTime, diffUTCTime,
+                                            getCurrentTime, secondsToDiffTime,
+                                            secondsToDiffTime)
 import           Network.HTTP.Types.Status
 import           Web.Cookie                (parseCookiesText)
 import           Web.JWT
 import           Web.Scotty                (ActionM, param, redirect, setHeader,
                                             status)
+
+epoch :: UTCTime
+epoch = UTCTime (fromGregorian 1970 1 1) (secondsToDiffTime 0)
+
+sessionDuration :: NominalDiffTime
+sessionDuration = fromInteger $ 60 * 60 * 60
 
 register :: ActionM ()
 register = do
@@ -41,7 +50,13 @@ unauthorize =
 
 authorize :: Text -> ActionM ()
 authorize username = do
-  let cs = def { sub = stringOrURI username }
+  now <- liftIO getCurrentTime
+  let jwtNbf = intDate $ fromInteger 0
+  let jwtExp = intDate $ (diffUTCTime now epoch) + sessionDuration
+  let cs = def {
+          sub = stringOrURI username
+        , nbf = jwtNbf
+        , Web.JWT.exp = jwtExp }
   let jwt = encodeSigned HS256 key cs
   setHeader "Set-Cookie" $ "session=" <>
     (TL.fromStrict jwt) <> "; path=/; HttpOnly"
@@ -50,9 +65,9 @@ authorize username = do
 isCurrentlyValid :: JWT VerifiedJWT -> UTCTime -> Bool
 isCurrentlyValid tkn now =
   let cl = claims tkn
-      toUTC diff = addUTCTime (secondsSinceEpoch diff) $ UTCTime (fromGregorian 1970 1 1) (secondsToDiffTime 0)
-      notBefore  = (now <=) . toUTC <$> nbf cl
-      notExpired = (now >=) . toUTC <$> Web.JWT.exp cl
+      toUTC diff = addUTCTime (secondsSinceEpoch diff) epoch
+      notBefore  = (now >=) . toUTC <$> nbf cl
+      notExpired = (now <=) . toUTC <$> Web.JWT.exp cl
       valid = (&&) <$> notBefore <*> notExpired
   in Just True == valid
 
